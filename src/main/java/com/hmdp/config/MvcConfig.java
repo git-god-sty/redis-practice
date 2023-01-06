ad75e7fb124526fbca6dcc9adf7e063cdb07b4df
苏天宇 <su.tianyu2@iwhalecloud.com>
2023-01-06 15:24:17 +0800

    基于session实现登录功能（发送短信验证码 & 验证码登录.注册 & 校验登录状态）

---
+++ src/main/java/com/hmdp/config/MvcConfig.java
@@ -0,0 +1,35 @@
+package com.hmdp.config;
+
+import com.hmdp.utils.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * TODO
+ *
+ * @className: MvcConfig
+ * @author: sutianyu
+ * @date: 2022-12-27 17:37
+ **/
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // 登录拦截器
+        registry.addInterceptor(new LoginInterceptor())
+                .excludePathPatterns(
+                        "/shop/**",
+                        "/voucher/**",
+                        "/shop-type/**",
+                        "/upload/**",
+                        "/blog/hot",
+                        "/user/code",
+                        "/user/login"
+                ).order(1);
+    }
+
+
+}
