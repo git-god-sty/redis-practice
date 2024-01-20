6a1ea71f52618f2f7db9000de68c4dace712573f
苏天宇 <su.tianyu2@iwhalecloud.com>
2024-01-20 10:24:55 +0800

    实现秒杀下单

--- src/main/java/com/hmdp/service/IVoucherOrderService.java
+++ src/main/java/com/hmdp/service/IVoucherOrderService.java
@@ -1,7 +1,9 @@
 package com.hmdp.service;
 
+import com.hmdp.dto.Result;
 import com.hmdp.entity.VoucherOrder;
 import com.baomidou.mybatisplus.extension.service.IService;
+import org.springframework.web.bind.annotation.PathVariable;
 
 /**
  * <p>
@@ -12,5 +14,7 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2021-12-22
  */
 public interface IVoucherOrderService extends IService<VoucherOrder> {
+
+    Result seckillVoucher(Long voucherId);
 
 }
